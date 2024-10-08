36c24d86efa9551b6e94c04aa31c37e1cd1d6575
extrymes <65893656+extrymes@users.noreply.github.com>
2024-10-09 01:50:22 +0200

    chore: replace mandatory files by bonus files

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 15:19:31 by sabras            #+#    #+#             */
-/*   Updated: 2024/05/24 13:49:50 by sabras           ###   ########.fr       */
+/*   Updated: 2024/10/09 01:48:56 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 char	*get_next_line(int fd)
 {
-	static char		*tab;
+	static char		*tab[1025];
 	char			*buffer;
 	int				bytes_read;
 	int				len;
 
 	if (BUFFER_SIZE < 1)
 		return (NULL);
-	len = ft_find_new_line(tab, BUFFER_SIZE);
+	len = ft_find_new_line(tab[fd], BUFFER_SIZE);
 	while (len == -1)
 	{
 		buffer = malloc((BUFFER_SIZE) * sizeof(char));
 		if (!buffer)
-			return (ft_tab_free(&tab), NULL);
+			return (ft_tab_free(&tab[fd]), NULL);
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		if (bytes_read == -1 || !ft_tab_add(&tab, buffer, bytes_read))
-			return (ft_tab_free(&tab), free(buffer), NULL);
-		len = ft_find_new_line(tab, bytes_read);
+		if (bytes_read == -1 || !ft_tab_add(&tab[fd], buffer, bytes_read))
+			return (ft_tab_free(&tab[fd]), free(buffer), NULL);
+		len = ft_find_new_line(tab[fd], bytes_read);
 		if (len == 0)
-			return (ft_tab_free(&tab), NULL);
+			return (ft_tab_free(&tab[fd]), NULL);
 	}
-	return (ft_create_line(&tab, len));
+	return (ft_create_line(&tab[fd], len));
 }
